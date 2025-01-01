3599fb2bd070b7d1fe8fe6edb9ac6ed6a8162897
Maes95 <michel.maes95@gmail.com>
2025-01-02 00:46:38 +0100

    Update bd_ejem2 to use web

--- spring_data/bd_ejem2/src/main/java/es/codeurjc/board/PostController.java
+++ spring_data/bd_ejem2/src/main/java/es/codeurjc/board/PostController.java
@@ -1,25 +1,17 @@
 package es.codeurjc.board;
 
-import java.net.URI;
-import java.util.Collection;
 import java.util.Optional;
 
-import jakarta.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
-@RestController
-@RequestMapping("/posts")
+import jakarta.annotation.PostConstruct;
+
+@Controller
 public class PostController {
 
 	@Autowired
@@ -30,58 +22,75 @@ public class PostController {
 		posts.save(new Post("Pepe", "Vendo moto", "Barata, barata"));
 		posts.save(new Post("Juan", "Compro coche", "Pago bien"));
 	}
-	
+
 	@GetMapping("/")
-	public Collection<Post> getPosts() {
-		return posts.findAll();
+	public String showPosts(Model model) {
+
+		model.addAttribute("posts", posts.findAll());
+
+		return "index";
 	}
 
-	@GetMapping("/{id}")
-	public ResponseEntity<Post> getPost(@PathVariable long id) {
+	@GetMapping("/post/{id}")
+	public String showPost(Model model, @PathVariable long id) {
 
-		Optional<Post> post = posts.findById(id);
+		Optional<Post> op = posts.findById(id);
 
-		if (post.isPresent()) {
-			return ResponseEntity.ok(post.get());
+		if (op.isPresent()) {
+			Post post = op.get();
+			model.addAttribute("post", post);
+			return "show_post";	
 		} else {
-			return ResponseEntity.notFound().build();
+			// return "redirect:/";
+			return "post_not_found";
 		}
 	}
 
-	@PostMapping("/")
-	public ResponseEntity<Post> createPost(@RequestBody Post post) {
+	@PostMapping("/post/new")
+	public String newPost(Model model, Post post) {
 
 		posts.save(post);
 
-		URI location = fromCurrentRequest().path("/{id}").buildAndExpand(post.getId()).toUri();
-
-		return ResponseEntity.created(location).body(post);
+		return "saved_post";
 	}
 
-	@PutMapping("/{id}")
-	public ResponseEntity<Post> replacePost(@PathVariable long id, @RequestBody Post newPost) {
+	@GetMapping("/editpost/{id}")
+	public String editBook(Model model, @PathVariable long id) {
 
-		if (posts.existsById(id)) {
-
-			newPost.setId(id);
-			posts.save(newPost);
-
-			return ResponseEntity.ok(newPost);
+		Optional<Post> op = posts.findById(id);
+		if (op.isPresent()) {
+			Post post = op.get();
+			model.addAttribute("post", post);
+			return "edit_post_page";
 		} else {
-			return ResponseEntity.notFound().build();
+			return "post_not_found";
 		}
 	}
 
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Post> deletePost(@PathVariable long id) {
+	@PostMapping("/editpost")
+	public String editBookProcess(Model model, Post editedPost) {
+
+		Optional<Post> op = posts.findById(editedPost.getId());
+		if (op.isPresent()) {
+			posts.save(editedPost);
+			model.addAttribute("post", editedPost);
+			return "edited_post";
+		} else {
+			return "post_not_found";
+		}
+	}
+
+	@PostMapping("/post/{id}/delete")
+	public String deletePost(Model model, @PathVariable long id) {
 
 		Optional<Post> post = posts.findById(id);
 
 		if (post.isPresent()) {
 			posts.deleteById(id);
-			return ResponseEntity.ok(post.get());
+			return "deleted_post";
 		} else {
-			return ResponseEntity.notFound().build();
+			return "post_not_found";
 		}
 	}
+
 }
