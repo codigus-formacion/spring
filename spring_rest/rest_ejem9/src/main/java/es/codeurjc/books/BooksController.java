2f69eed76d6406f1aa8b500823d80971fb3c3baa
Micael Gallego <micael.gallego@gmail.com>
2025-02-28 00:08:06 +0100

    Update server REST client examples

--- spring_rest/rest_ejem9/src/main/java/es/codeurjc/books/BooksController.java
+++ spring_rest/rest_ejem9/src/main/java/es/codeurjc/books/BooksController.java
@@ -1,42 +1,39 @@
 package es.codeurjc.books;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @RestController
 public class BooksController {
 
-	record BooksResponse(List<Book> items) {
-	}
+	@GetMapping("/booktitles")
+	public List<String> getBookTitles(@RequestParam String title) {
 
-	record Book(VolumeInfo volumeInfo) {
-	}
+		RestClient restClient = RestClient.create();
 
-	record VolumeInfo(String title) {
-	}
-
-    @GetMapping("/booktitles")
-    public List<String> getBookTitles(@RequestParam String title) {
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        String url = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + title;
-
-        BooksResponse data = restTemplate.getForObject(url, BooksResponse.class);
+        ObjectNode data = restClient.get()
+            .uri("https://www.googleapis.com/books/v1/volumes?q=intitle:" + title)
+            .retrieve()
+            .body(ObjectNode.class);
 
         List<String> bookTitles = new ArrayList<>();
 
-        for (Book book : data.items()) {
-            bookTitles.add(book.volumeInfo().title());
-        }
+		ArrayNode items = (ArrayNode) data.get("items");
+		for (int i = 0; i < items.size(); i++) {
+			JsonNode item = items.get(i);
+			String bookTitle = item.get("volumeInfo").get("title").asText();
+			bookTitles.add(bookTitle);
+		}
 
-        return bookTitles;
-    }
+		return bookTitles;
+	}
 }
