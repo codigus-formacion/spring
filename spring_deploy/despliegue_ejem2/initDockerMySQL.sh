ca80aaee1165375d74c9ae232a06cfc5bd77a2f6
Michel Maes <michel.maes95@gmail.com>
2025-11-27 18:13:56 +0100

    Update deploy examples

--- spring_deploy/despliegue_ejem2/initDockerMySQL.sh
+++ spring_deploy/despliegue_ejem2/initDockerMySQL.sh
@@ -1,1 +1,2 @@
-docker run --rm -e MYSQL_ROOT_PASSWORD=password -e  MYSQL_DATABASE=posts -p 3306:3306 -d mysql:8.0.33
+docker run --rm -e MYSQL_ROOT_PASSWORD=password \
+ -e MYSQL_DATABASE=posts -p 3306:3306 -d mysql:9.5.0
